09955964dbfa4daac2557d58e739ff8b9f15fd2a
Mike Moore <mikemoore1618@gmail.com>
2018-06-22 16:19:05 -0700

    search for movie displays movie poster results for that key word

--- server.js
+++ server.js
@@ -15,27 +15,19 @@ app.use(express.static('public'));
 //contact the OMDB api (user the OMDB documentation), searching for whatever the user included in their request as :term
 //when the results get returned to your app, go through the results and send the user an HTML string of <img> tags of all the movie posters for the search results.
 //By the end, the user should be able to navigate to localhost:3000/search/terminator from their browser, and see all of the movie posters for the search results.
-app.get('/', (req, res) => {
+app.get('/search/:term', (req, res) => {
     console.log(process.env.API_KEY)
-    const apiUrl = `http://omdbapi.com/?apikey=${process.env.API_KEY}&s=star_wars`
+    const apiUrl = `http://omdbapi.com/?apikey=${process.env.API_KEY}&s=${req.params.term}`
 
     apiClient({ method: 'get', url: apiUrl }).then((apiResponse) => {
-
-        res.send(`<img src=${apiResponse.data.Search[0].Poster}>`)
-
-
-        // let results = ''
-        // apiResponse.data.data.forEach((r) => {
-        //     const imgUrl = r.images.original.url
-        //     results += `<img src="${imgUrl}">`
-        // })
-        // res.send(results)
+        let results = ''
+        apiResponse.data.Search.forEach((r) => {
+            const imgUrl = r.Poster
+            results += `<img src="${imgUrl}">`
+        })
+        res.send(results)
     })
-})
-// app.get('/', (req,res) => {
-//     res.sendFile(__dirname+'/views/index.html');
-// })
-
+});
 
 app.listen(PORT, (err) => {
     console.log(err || `Server running on ${PORT} :)`)
