18ad23f401b642f2cc7e8451f552da1b629b450c
Mike Moore <mikemoore1618@gmail.com>
2018-06-22 15:55:23 -0700

    get route for index

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const
   express = require('express'),
   app = express(),
-  logger = require('morgan'),
   axios = require('axios'),
   PORT = 3000
 
@@ -12,10 +11,30 @@ const apiClient = axios.create()
 
 app.use(express.static('public'));
 
+//get route for "/search/:term" which will:
+//contact the OMDB api (user the OMDB documentation), searching for whatever the user included in their request as :term
+//when the results get returned to your app, go through the results and send the user an HTML string of <img> tags of all the movie posters for the search results.
+//By the end, the user should be able to navigate to localhost:3000/search/terminator from their browser, and see all of the movie posters for the search results.
+app.get('/', (req, res) => {
+    console.log(process.env.API_KEY)
+    const apiUrl = `http://omdbapi.com/?apikey=${process.env.API_KEY}&s=star_wars`
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname+'/views/index.html');
+    apiClient({ method: 'get', url: apiUrl }).then((apiResponse) => {
+
+        res.send(`<img src=${apiResponse.data.Search[0].Poster}>`)
+
+
+        // let results = ''
+        // apiResponse.data.data.forEach((r) => {
+        //     const imgUrl = r.images.original.url
+        //     results += `<img src="${imgUrl}">`
+        // })
+        // res.send(results)
+    })
 })
+// app.get('/', (req,res) => {
+//     res.sendFile(__dirname+'/views/index.html');
+// })
 
 
 app.listen(PORT, (err) => {
